5f263609bce763147b390886f387135b0af7003c
valentin <vb.brebionpro@gmail.com>
2023-11-17 10:48:04 +0100

    fetch date refresh

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import TemperatureDisplay from '../../src/components/TemperatureDisplay';
 import WeatherCode from '../../src/components/WeatherCode';
 
@@ -6,26 +6,35 @@ import WeatherCode from '../../src/components/WeatherCode';
 
 
 const App = () => {
+  const [apimeteo, setApiMeteo] = useState(null)
+  const [lastUpdate, setLastUpdate] = useState(null);
 
   const FetchAPImeteo = () => {
     const latitude = 46.1592
     const longitude = -1.171
     const timezone = 'Europe/London'
-    const [fact, setFact] = useState(null)
-    const getData = () => {
+  
 
-        fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}`)
-            .then(res => res.json())
-            .then(data => setFact(data))
-    }
+    fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}`)
+    .then(res => res.json())
+    .then(data => {
+      setApiMeteo(data);
+      // Mettez à jour le timestamp de la dernière mise à jour
+     setLastUpdate(Date.now());
+    });
+
   }
+  useEffect(() => {
+    // Mettez à jour la date lors du premier rendu
+    FetchAPImeteo();
+  }, []);
   
   
   return <main className="weather-container">
     <div className="weather-container-content">
       <header className="weather-container-header">
         <p className="location">La Rochelle</p>
-        <button className="refresh-button" onClick={FetchAPImeteo()}>
+        <button className="refresh-button" onClick={FetchAPImeteo}>
           <img src="https://lpmiaw-react.napkid.dev/img/weather/refresh.png" alt="Refresh" />
         </button>
       </header>
@@ -89,7 +98,7 @@ const App = () => {
         </ul>
       </section>
       <footer className="weather-container-footer">
-        <p>Mis à jour à 10:14:21</p>
+        <p>{lastUpdate && new Date(lastUpdate).toLocaleString()}</p>
       </footer>
     </div>
   </main>
